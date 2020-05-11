d4a5fc4b376d25f6d9fe875b672a26a90ed662a7
unknown <hello@jeffdelaney.me>
2020-05-11 08:24:43 -0700

    add video to home page

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -84,21 +84,33 @@ function App() {
 function Home() {
   return (
     <>
-      <h2>Stripe React + Node.js</h2>
-      <p>
-        Learn how to build a fullstack payment solution with Stripe, React, and
-        Node.js.
-      </p>
+      <div class="well">
+        <h2>Stripe React + Node.js Live Demo</h2>
+      </div>
 
-      <h2>Running in Test Mode</h2>
-      <p>
-        This demo is running in Stripe test mode, so feel free to submit
-        payments with testing cards.
-      </p>
-      <p>
-        Reference the <a href="https://github.com/fireship-io/stripe-payments-js-course">source code</a> and watch the{' '}
-        <a href="https://fireship.io/courses/stripe-js">Full Stripe JavaScript Course</a>.{' '}
-      </p>
+      <div class="embed-responsive embed-responsive-16by9 vid">
+        <iframe
+          src="https://player.vimeo.com/video/416381401"
+          // width="640"
+          // height="360"
+          frameborder="0"
+          allow="autoplay; fullscreen"
+          allowfullscreen></iframe>
+      </div>
+
+      <div class="well">
+        <h2>Running in Test Mode</h2>
+        <p>
+          This demo is running in Stripe test mode, so feel free to submit
+          payments with testing cards.
+        </p>
+        <a class="btn btn-outline-success"  href="https://fireship.io/courses/stripe-js">
+            Full Stripe JS Course
+          </a>
+          <a class="btn btn-secondary" href="https://github.com/fireship-io/stripe-payments-js-course">
+            source code
+          </a>
+      </div>
     </>
   );
 }
